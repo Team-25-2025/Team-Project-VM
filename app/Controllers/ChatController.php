ba0b4544cc672d3c95a0fe5c858b195f882fd0eb
Ivan Chen <ivchen@gmail.com>
2026-02-03 16:56:42 +0000

    Refactor TeamBoardController to require teamId for view rendering
    Deleted Chat

--- app/Controllers/ChatController.php
+++
@@ -1,7 +0,0 @@
-<?php 
-namespace App\Controllers;
-class ChatController {
-  public function index() {
-    view('chat/chat_employee.view.php');
-  }
-}
--- app/Controllers/TeamBoardController.php
+++ app/Controllers/TeamBoardController.php
@@ -2,13 +2,15 @@
 namespace App\Controllers;
 
 class TeamBoardController {
-  public function index() {
-    if ($_SESSION['user']['permission'] == 'manager') {
-      view('TeamBoard/team_board_hub_manager.view.php');  
-    } elseif ($_SESSION['user']['permission'] == 'teamleader') {
-      view('TeamBoard/team_board_teamleader.view.php');
-    } elseif ($_SESSION['user']['permission'] == 'employee') {
-      view('TeamBoard/team_board_employee.view.php');
-    } 
+  public function index()
+  {
+    if (!isset($_GET['teamId'])) {
+      die("No team selected.");
+    }
+    $teamId = $_GET['teamId'];
+    // Pass $teamId to the view
+    view('projectsAndTeamBoard/teamboard.php', ['teamId' => $teamId]);
   }
+
 }
+?>
